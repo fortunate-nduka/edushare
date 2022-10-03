4abdaabc79cdb6cdfb54535efeda57301d3e8a04
Fortunate ogodu <ogodujoseph@gmail.com>
2022-10-03 10:01:50 +0100

    increased the logo, menu & button sizes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ text, bg, textColor,classes }) => {
     return (
-        <button className={`${bg} ${textColor} px-6 py-2 rounded-md ${classes}`}>{text}</button>
+        <button className={`${bg} ${textColor} px-6 py-3 rounded-md ${classes}`}>{text}</button>
     )
 }
 export default Button
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,11 +6,11 @@ import Button from './Button'
 
 const Nav = () => {
     return (
-        <nav className='flex items-center justify-between py-5'><img src={Logo} className="w-24 sm:w-28 md:w-32 lg:w-36" alt="" />
+        <nav className='flex items-center justify-between py-5'><img src={Logo} className="w-28 lg:w-36" alt="" />
             <ul className='hidden items-center md:flex gap-9'>
                 {navlinks.map(link =>
                     (<li key={uuid()} className='text-black'>{link.title}</li>))}</ul>
-            <HiMenu className='w-7 h-7 md:hidden' />
+            <HiMenu className='w-8 h-8 md:hidden' />
             <Button text='Sign in' bg='bg-lightBlue' textColor='text-blue' classes='hidden md:block' />
         </nav>
     )
